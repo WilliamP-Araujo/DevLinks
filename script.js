01621395622370dca43cd1ec1c4b9f640344784a
William Ponte Araujo <williampontearaujo@gmail.com>
2024-09-11 21:20:46 -0300

    Sistema de idiomas adicionado

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
+const texts = {
+  en: {
+      username: "@Uwill.png",
+      linkedin: "See my LinkedIn",
+      projectCode: "Code of this project",
+      community: "Meet my community",
+      pythonProjects: "See my Python projects!",
+      footer: "Made with ♥ by William P."
+  },
+  pt: {
+      username: "@Uwill.png",
+      linkedin: "Veja meu LinkedIn",
+      projectCode: "Código deste projeto",
+      community: "Conheça minha comunidade",
+      pythonProjects: "Veja meus projetos em Python!",
+      footer: "Feito com ♥ por William P."
   }
+};
+
+function changeLanguage() {
+  const selectedLanguage = document.getElementById('languageSelector').value;
+  document.getElementById('username').innerText = texts[selectedLanguage].username;
+  document.getElementById('linkedin').innerText = texts[selectedLanguage].linkedin;
+  document.getElementById('projectCode').innerText = texts[selectedLanguage].projectCode;
+  document.getElementById('community').innerText = texts[selectedLanguage].community;
+  document.getElementById('pythonProjects').innerText = texts[selectedLanguage].pythonProjects;
+  document.getElementById('footer').innerText = texts[selectedLanguage].footer;
+}
+
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+}
